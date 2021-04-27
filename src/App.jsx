ab7385cbdf13cf8083ea5ec207cbafc4e2e3e33f
Darsh <darsh2987@gmail.com>
2021-04-27 13:13:59 +0100

    async await axios function - get random item from api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from "react";
 import "./App.scss";
+import Axios from "axios";
+
+// Components
+import Card from "./components/Card.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await Axios("http://leads.beta.openstudycollege.info/getTopLeads");
+        const results = response.data;
+        const randomItem = results[Math.floor(Math.random() * results.length)];
+        setData(randomItem);
+      } catch (e) {
+        console.log("ERROR FETCHING DATA FROM API");
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <Card data={data} />
+    </div>
+  );
 }
 
 export default App;
