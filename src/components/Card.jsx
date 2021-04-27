1ffe9d6fe1688d6a2cc19b8fcd198cf3e975b3d3
Darsh <darsh2987@gmail.com>
2021-04-27 16:20:13 +0100

    moved card header into its own component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
-import profilePicture from "../images/profile-picture.jpg";
 
 // Components
 import Spinner from "./Spinner.jsx";
+import CardHeader from "./CardHeader.jsx";
 
 function Card(props) {
   return props.isLoading ? (
     <Spinner />
   ) : (
     <div className="card">
-      <div className="card_header">
-        <FontAwesomeIcon icon={faTimes} />
-        <div className="card_profile">
-          <img src={profilePicture} className="card_profile-picture" alt="Profile" />
-          <FontAwesomeIcon icon={faCheck} />
-          <h1 className="card_profile-name">{props.data.name}</h1>
-          <h3 className="card_profile-status">{props.data.status}</h3>
-          <h3 className="card_profile-id">ID: {props.data.id}</h3>
-        </div>
-      </div>
+      <CardHeader data={props.data} />
     </div>
   );
 }
---
+++ src/components/CardHeader.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import profilePicture from "../images/profile-picture.jpg";
+
+function CardHeader(props) {
+  return (
+    <div className="card_header">
+      <FontAwesomeIcon icon={faTimes} />
+      <div className="card_profile">
+        <div className="card_profile-picture-wrapper">
+          <img src={profilePicture} className="card_profile-picture" alt="Profile" />
+          <FontAwesomeIcon icon={faCheck} />
+        </div>
+
+        <h1 className="card_profile-name">{props.data.name}</h1>
+        <h3 className="card_profile-status">{props.data.status}</h3>
+        <h3 className="card_profile-id">ID: {props.data.id}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default CardHeader;
