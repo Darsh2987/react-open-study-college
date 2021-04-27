18eb3f9c8d7e4b58cd77ebe755bed68fd4014d85
Darsh <darsh2987@gmail.com>
2021-04-27 13:25:13 +0100

    import and apply logic for spinner/loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+// Components
+import Spinner from "./Spinner.jsx";
+
 function Card(props) {
-  return (
+  return props.isLoading ? (
+    <Spinner />
+  ) : (
     <div className="card">
       <h1>{props.data.name}</h1>
     </div>
