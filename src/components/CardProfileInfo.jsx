4782999ee0c6571937dfec86892e58601c47c589
Darsh <darsh2987@gmail.com>
2021-04-27 18:28:00 +0100

    correcting class names

--- src/components/CardProfileInfo.jsx
+++ src/components/CardProfileInfo.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function CardProfileInfo(props) {
   return (
-    <div className="profile-info">
-      <div className="profile-info_contact">
+    <div className="card_profile-info">
+      <div className="card_profile-info-contact">
         <h2>Contact Details</h2>
         <p>Email: {props.data.email}</p>
         <p>Tel: {props.data.telephone}</p>
       </div>
-      <div className="profile-info_about">
+      <div className="card_profile-info-about">
         <h2>About Me</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit obcaecati, dolorum vel neque unde quod fugiat.</p>
       </div>
