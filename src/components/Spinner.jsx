87aa3e8c883f2dfce590b0eeb0a0519fe86ac42a
Darsh <darsh2987@gmail.com>
2021-04-27 13:24:17 +0100

    spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import spinner from "../images/spinner.gif";
+
+function Spinner() {
+  return <img src={spinner} className="spinner" alt="Loading" />;
+}
+
+export default Spinner;
