20d58c35aae1d50ef6040442f192a224aadb1ed1
Darsh <darsh2987@gmail.com>
2021-04-27 17:40:38 +0100

    car info bar markup

---
+++ src/components/CardInfoBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CardInfoBar() {
+  return (
+    <div className="card_infobar">
+      <div className="card_infobar-col-one">
+        <p>Complete</p>
+        <p>Enrolment Status</p>
+      </div>
+      <span></span>
+      <div className="card_infobar-col-two">
+        <p>3</p>
+        <p>Completed Courses</p>
+      </div>
+    </div>
+  );
+}
+
+export default CardInfoBar;
