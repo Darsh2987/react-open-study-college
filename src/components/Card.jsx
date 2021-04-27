098b4231950de79cd42de4437157cb76dd6707a0
Darsh <darsh2987@gmail.com>
2021-04-27 16:05:48 +0100

    car header markup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import profilePicture from "../images/profile-picture.jpg";
 
 // Components
 import Spinner from "./Spinner.jsx";
@@ -8,7 +11,16 @@ function Card(props) {
     <Spinner />
   ) : (
     <div className="card">
-      <h1>{props.data.name}</h1>
+      <div className="card_header">
+        <FontAwesomeIcon icon={faTimes} />
+        <div className="card_profile">
+          <img src={profilePicture} className="card_profile-picture" alt="Profile" />
+          <FontAwesomeIcon icon={faCheck} />
+          <h1 className="card_profile-name">{props.data.name}</h1>
+          <h3 className="card_profile-status">{props.data.status}</h3>
+          <h3 className="card_profile-id">ID: {props.data.id}</h3>
+        </div>
+      </div>
     </div>
   );
 }
