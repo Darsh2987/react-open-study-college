02c0c762919078ee11f6ae239c679e27bd89561c
Darsh <darsh2987@gmail.com>
2021-04-28 19:13:13 +0100

    email and phone validation reg ex

--- src/components/CardProfileInfo.jsx
+++ src/components/CardProfileInfo.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 
 function CardProfileInfo(props) {
+  // Email Validation
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  // Phone Number Validate
+  function validatePhone(phone) {
+    const regex = /^([+|\d])+([\s|\d])+([\d])$/;
+    return regex.test(phone);
+  }
+
+  console.log(props.data.email);
+  console.log(props.data.telephone);
+
   return (
     <div className="card_profile-info">
       <div className="card_profile-info-contact">
         <h2>Contact Details</h2>
-        <p>Email: {props.data.email || "TBA"}</p>
+        <p>Email: {validateEmail(props.data.email) ? props.data.email : "Invalid Email"}</p>
         <br />
-        <p>Tel: {props.data.telephone || "TBA"}</p>
+        <p>Tel: {validatePhone(props.data.telephone) ? props.data.telephone : "Invalid Phone No."}</p>
       </div>
       <div className="card_profile-info-about">
         <h2>About Me</h2>
